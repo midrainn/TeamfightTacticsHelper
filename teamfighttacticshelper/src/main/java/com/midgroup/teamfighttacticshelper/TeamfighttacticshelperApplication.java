59b3046c2e72d49492f94b563322058a063638d3
mid <673601015@qq.com>
2019-07-20 19:43:29 +0800

    。
     。

---
+++ teamfighttacticshelper/src/main/java/com/midgroup/teamfighttacticshelper/TeamfighttacticshelperApplication.java
@@ -0,0 +1,13 @@
+package com.midgroup.teamfighttacticshelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamfighttacticshelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamfighttacticshelperApplication.class, args);
+    }
+
+}
---
+++ teamfighttacticshelper/src/test/java/com/midgroup/teamfighttacticshelper/TeamfighttacticshelperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.midgroup.teamfighttacticshelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeamfighttacticshelperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
